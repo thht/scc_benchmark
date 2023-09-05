1700a037e8e2941cd4f66742333c4ad80e049891
Thomas Hartmann <thomas.hartmann@th-ht.de>
2023-09-05 15:00:39 +0200

    better login

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 mkdir images
 
-echo "$secrets.GITHUB_TOKEN" | apptainer remote login -u "$secrets.GHCR_USERNAME" --password-stdin oras://ghcr.io
-
 apptainer build images/phoronix.sif phoronix.def
 apptainer push images/phoronix.sif oras://ghcr.io/${GITHUB_REPOSITORY}/benchmarks:latest
