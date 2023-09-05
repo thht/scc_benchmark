7b79edffa942fe17a2e9b06cbd4cc081ad4c549f
Thomas Hartmann <thomas.hartmann@th-ht.de>
2023-09-05 14:37:43 +0200

    first try

---
+++ build.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+mkdir images
+
+echo ${{ secrets.GITHUB_TOKEN }} | apptainer remote login -u ${{ secrets.GHCR_USERNAME }} --password-stdin oras://ghcr.io
+
+apptainer build images/phoronix.sif phoronix.def
+apptainer push images/phoronix.sif oras://ghcr.io/${GITHUB_REPOSITORY}/benchmarks:latest
